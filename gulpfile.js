eceb87188415a83676052fb91f4514809b733954
CheITdevelopers <atroshchenko.ivan@gmail.com>
2017-12-25 15:04:12 +0200

    add del

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,62 @@
 'use strict';
 
 var gulp = require('gulp'),
+    rimraf = require('rimraf'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     concatCss = require('gulp-concat-css'),
-    minifyCSS = require('gulp-minify-css'),
+    cleanCss = require('gulp-clean-css'),
     uglyfly = require('gulp-uglyfly'),
     concat = require('gulp-concat'),
     imagemin = require('gulp-imagemin'),
     imageminJpegRecompress = require('imagemin-jpeg-recompress'),
     imageminPngquant = require('imagemin-pngquant'),
     svgSprite = require("gulp-svg-sprites");
+    //browserSync = require("browser-sync"),
+    //reload = browserSync.reload
 
-var pathSource = './_src/';
-var pathDestination = './dist/';
+var src = './_src/';
+var dist = './dist/';
 
-gulp.task('sass', function () {
-    return gulp.src(pathSource + 'scss/**/*.scss')
+gulp.task('rimraf', function (cb) {
+   rimraf(dist, cb);
+});
+
+gulp.task('sassDev', function () {
+    return gulp.src(src + 'scss/**/*.scss')
        .pipe(sourcemaps.init())
        .pipe(sass().on('error', sass.logError))
-       .pipe(sourcemaps.write('/'))
-       .pipe(gulp.dest('css'));
+       .pipe(sourcemaps.write())
+       .pipe(gulp.dest(dist + 'css/'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src(src + 'scss/**/*.scss')
+       .pipe(sass().on('error', sass.logError))
+       .pipe(concatCss('main.css'))
+       .pipe(cleanCss({keepBreaks: false}))
+       .pipe(gulp.dest(dist + 'css/'));
 });
 
 gulp.task('css', function() {
-    gulp.src(pathSource + '/css/*.css')
-        .pipe(concatCss(pathDestination + 'css/bundle.css'))
-        .pipe(minifyCSS({keepBreaks: false}))
-        .pipe(gulp.dest(pathDestination + 'css/'));
+    gulp.src(src + 'css/*.css')
+       .pipe(concatCss(dist + 'css/main.css'))
+       .pipe(cleanCss({keepBreaks: false}))
+       .pipe(gulp.dest(dist + 'css/'));
 });
 
 gulp.task('js', function() {
-    gulp.src(pathSource + 'js/*.js')
+    gulp.src(src + 'js/*.js')
         .pipe(concat('bundle.js'))
         .pipe(uglyfly())
-        .pipe(gulp.dest(pathDestination + 'js/'))
+        .pipe(gulp.dest(dist + 'js/'))
+});
+
+gulp.task('devJs', function() {
 });
 
 function imagesTask(folder, folderOut) {
-gulp.src(pathSource + folder)
+gulp.src(src + folder)
 	.pipe(imagemin([
             imagemin.gifsicle({interlaced: true}),
             imageminJpegRecompress({
@@ -49,26 +67,26 @@ gulp.src(pathSource + folder)
             imageminPngquant({quality: '75-85'}),
             imagemin.svgo({plugins: [{removeViewBox: false}]})
 		]))
-	.pipe(gulp.dest(pathDestination + folderOut));
+	.pipe(gulp.dest(dist + folderOut));
 }
 
 gulp.task('images', function() {
-    imagesTask(pathSource + 'images/**', pathDestination + 'images/');
+    imagesTask(src + 'images/**', dist + 'images/');
 });
 
 gulp.task('uploads', function() {
-    imagesTask(pathSource + 'uploads/**', pathDestination + 'uploads/');    	    
+    imagesTask(src + 'uploads/**', dist + 'uploads/');    	    
 });
 
 gulp.task('sprites', function () {
-    return gulp.src(pathSource + 'sprites/*.svg')
+    return gulp.src(src + 'sprites/*.svg')
         .pipe(svgSprite({mode: "symbols"}))
-        .pipe(gulp.dest(pathDestination + 'images/sprites/'));
+        .pipe(gulp.dest(dist + 'sprites/'));
 });
 
-gulp.task('default', ['sass', 'css', 'js']);
-gulp.task('production', ['css', 'js', 'images', 'uploads', 'sprites']);
-
+gulp.task('del', ['rimraf']);
+gulp.task('default', ['devSass', 'css', 'js', 'images', 'uploads', 'sprites']);
 gulp.task('watch', function () {
-  gulp.watch(pathSource + 'scss/**/*.scss', ['sass', 'css', 'js']);
+  gulp.watch(src + 'scss/**/*.scss', ['devSass', 'devJs']);
 });
+gulp.task('prod', ['sass', 'css', 'js', 'images', 'uploads', 'sprites']);
